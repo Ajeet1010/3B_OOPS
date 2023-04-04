ba5a7a292d0e15c5bf9143fe26489a7bd559a7f9
Ajeet <ajeetpal101999@gmail.com>
2023-04-05 03:16:07 +0530

    [Ajeet] Constructor, Access Modifiers & Interfaces.

---
+++ 3B_OOPS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3B_OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  1. Constructor  2. Access Modifiers   3. Interface  *****");
+        }
+    }
+}
