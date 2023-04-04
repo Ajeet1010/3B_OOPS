483355634d1424932a95da17bfe1f02e7860a531
Ajeet <ajeetpal101999@gmail.com>
2023-04-05 03:38:05 +0530

    [Ajeet] Constructor.

---
+++ 3B_OOP/Constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3B_OOP
+{
+    // UC1- Constructor
+    internal class Constructor
+    {
+        // fields or members.
+        public int ID;
+        public string Name;
+
+        //1. Empty constructor
+        public Constructor()
+        {
+            
+        }
+        public Constructor(string x)
+        {
+            Name = x;
+
+        }
+        public Constructor(int x, string y)
+        {
+            ID = x;
+            Name = y;
+        }
+    }
+}
--- 3B_OOP/Program.cs
+++ 3B_OOP/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  1. Constructor  2. Access Modifiers  3. Interfaces  *****");
+            Console.Write("Enter ur choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Constructor");
+                    Constructor constructor = new Constructor(1, "Aman");
+                    Console.WriteLine(constructor.ID  + "  " + constructor.Name);
+                    break;
+            }
+            
         }
     }
 }
